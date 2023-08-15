f6416302a7d6ae51cbbaffee59945067e1324c8b
Shobhith Lakkakula <shobhith.lakkakula@tcpwave.com>
2023-08-15 15:52:02 +0530

    first commit

---
+++ Spring-Boot-Application/src/main/java/com/spring/crud/SpringBootApplication/Application.java
@@ -0,0 +1,17 @@
+package com.spring.crud.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+/*
+ * MasterX branch
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
